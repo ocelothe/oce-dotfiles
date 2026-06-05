5361d0c11f835a066ee4a242ccd4eddb4b9008ef
ocelothe2k1 <wilczurpolska@gmail.com>
2026-06-05 16:35:21 +0200

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    #pfetch
+figlet -w 280 -f ANSI_Shadow "I use Nobara btw."|lolcat
+
+fastfetch --config /home/oce/Dokumenty/stuff/fastfetch/config.jsonc
+end
